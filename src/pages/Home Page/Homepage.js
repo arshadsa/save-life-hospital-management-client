5dbdfd850e3531b030a34f0a37b18d2bcb2e70e7
Emranul_farhad <emranulfarhad@gmail.com>
2022-10-16 19:24:03 +0600

    suitable work components and pages create

---
+++ src/pages/Home Page/Homepage.js
@@ -0,0 +1,11 @@
+// here we will add all of our home page components file 
+
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div>Homepage</div>
+  )
+}
+
+export default Homepage
