0abd190da3a8317948005541b73047ba73c530c9
Emranul_farhad <emranulfarhad@gmail.com>
2022-10-16 19:37:57 +0600

    aaa

--- src/pages/Home Page/Homepage.js
+++ src/pages/Home Page/Homepage.js
@@ -1,10 +1,15 @@
-// here we will add all of our home page components file 
+// here we will add all of our home page components file if you cannot understand please read our componennts text.txt file
+
 
 import React from 'react'
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <div>
+        {/* banner  */}
+        {/* what are you looking */}
+    </div>
+
   )
 }
 
