e7b1c3e78ca636423fc31b7768040cba2b467a1a
ShamimHosen75 <developer.shamimhosen@gmail.com>
2022-10-17 19:17:08 +0600

    changes

--- src/components/Home components/Specialties/Specialties.js
+++ src/components/Home components/Specialties/Specialties.js
@@ -1,10 +1,285 @@
 import React from 'react';
+import Img1 from '../../../assets/Speciality-img/medicine.png';
+
 
 const Specialties = () => {
   return (
-    <div>
-      <div className='text-4xl font-bold pt-9 pb-9'>
-        <h1>Please select a speciality</h1>
+    <div className='tpx-4 mx-auto max-w-lg sm:px-6 md:max-w-2xl md:px-8 lg:max-w-5xl xl:max-w-7xl 2xl:container'>
+      <h1 className='text-xl pb-8 font-bold text-gray-900 md:text-4xl'>Please select a speciality</h1>
+
+      {/* Specialties Card  */}
+      <div className='grid gap-6 sm:grid-cols-1 lg:grid-cols-3 2xl:grid-cols-3'>
+        {/* Card-Body-1  */}
+        <div className='tp-4 rounded-[10px] bg-white shadow ring-4 ring-gray-100 ring-offset-2 hover:bg-gray-50 hover:no-underline'>
+        <div className='tmb-5 flex space-x-4'>
+            {/* Card-img  */}
+            <div className='tmb-4 h-24 w-24 flex-shrink-0 rounded'>
+              <img src={Img1} alt="" className='h-24 w-24 object-cover'/>
+            </div>
+            {/* Card-Title  */}
+            <div>
+              <h3 className='text-xl tmb-2 font-semibold text-gray-900'>
+                Medicine / General Physician</h3>
+              <p className='text-sm m-0 pr-8 font-normal text-gray-600'>
+                Cold, flu, fever, vomiting, infections, headaches or any other general health issues
+              </p>
+            </div>
+        </div>
+        </div>
+     
+        {/* Card-Body-2  */}
+        <div className='tp-4 rounded-[10px] bg-white shadow ring-4 ring-gray-100 ring-offset-2 hover:bg-gray-50 hover:no-underline'>
+        <div className='tmb-5 flex space-x-4'>
+            {/* Card-img  */}
+            <div className='tmb-4 h-24 w-24 flex-shrink-0 rounded'>
+              <img src={Img1} alt="" className='h-24 w-24 object-cover'/>
+            </div>
+            {/* Card-Title  */}
+            <div>
+              <h3 className='text-xl tmb-2 font-semibold text-gray-900'>
+                Medicine / General Physician</h3>
+              <p className='text-sm m-0 pr-8 font-normal text-gray-600'>
+                Cold, flu, fever, vomiting, infections, headaches or any other general health issues
+              </p>
+            </div>
+        </div>
+        </div>
+     
+        {/* Card-Body-3  */}
+        <div className='tp-4 rounded-[10px] bg-white shadow ring-4 ring-gray-100 ring-offset-2 hover:bg-gray-50 hover:no-underline'>
+        <div className='tmb-5 flex space-x-4'>
+            {/* Card-img  */}
+            <div className='tmb-4 h-24 w-24 flex-shrink-0 rounded'>
+              <img src={Img1} alt="" className='h-24 w-24 object-cover'/>
+            </div>
+            {/* Card-Title  */}
+            <div>
+              <h3 className='text-xl tmb-2 font-semibold text-gray-900'>
+                Medicine / General Physician</h3>
+              <p className='text-sm m-0 pr-8 font-normal text-gray-600'>
+                Cold, flu, fever, vomiting, infections, headaches or any other general health issues
+              </p>
+            </div>
+        </div>
+        </div>
+     
+        {/* Card-Body-4  */}
+        <div className='tp-4 rounded-[10px] bg-white shadow ring-4 ring-gray-100 ring-offset-2 hover:bg-gray-50 hover:no-underline'>
+        <div className='tmb-5 flex space-x-4'>
+            {/* Card-img  */}
+            <div className='tmb-4 h-24 w-24 flex-shrink-0 rounded'>
+              <img src={Img1} alt="" className='h-24 w-24 object-cover'/>
+            </div>
+            {/* Card-Title  */}
+            <div>
+              <h3 className='text-xl tmb-2 font-semibold text-gray-900'>
+                Medicine / General Physician</h3>
+              <p className='text-sm m-0 pr-8 font-normal text-gray-600'>
+                Cold, flu, fever, vomiting, infections, headaches or any other general health issues
+              </p>
+            </div>
+        </div>
+        </div>
+     
+        {/* Card-Body-5  */}
+        <div className='tp-4 rounded-[10px] bg-white shadow ring-4 ring-gray-100 ring-offset-2 hover:bg-gray-50 hover:no-underline'>
+        <div className='tmb-5 flex space-x-4'>
+            {/* Card-img  */}
+            <div className='tmb-4 h-24 w-24 flex-shrink-0 rounded'>
+              <img src={Img1} alt="" className='h-24 w-24 object-cover'/>
+            </div>
+            {/* Card-Title  */}
+            <div>
+              <h3 className='text-xl tmb-2 font-semibold text-gray-900'>
+                Medicine / General Physician</h3>
+              <p className='text-sm m-0 pr-8 font-normal text-gray-600'>
+                Cold, flu, fever, vomiting, infections, headaches or any other general health issues
+              </p>
+            </div>
+        </div>
+        </div>
+     
+        {/* Card-Body-6  */}
+        <div className='tp-4 rounded-[10px] bg-white shadow ring-4 ring-gray-100 ring-offset-2 hover:bg-gray-50 hover:no-underline'>
+        <div className='tmb-5 flex space-x-4'>
+            {/* Card-img  */}
+            <div className='tmb-4 h-24 w-24 flex-shrink-0 rounded'>
+              <img src={Img1} alt="" className='h-24 w-24 object-cover'/>
+            </div>
+            {/* Card-Title  */}
+            <div>
+              <h3 className='text-xl tmb-2 font-semibold text-gray-900'>
+                Medicine / General Physician</h3>
+              <p className='text-sm m-0 pr-8 font-normal text-gray-600'>
+                Cold, flu, fever, vomiting, infections, headaches or any other general health issues
+              </p>
+            </div>
+        </div>
+        </div>
+     
+        {/* Card-Body-7  */}
+        <div className='tp-4 rounded-[10px] bg-white shadow ring-4 ring-gray-100 ring-offset-2 hover:bg-gray-50 hover:no-underline'>
+        <div className='tmb-5 flex space-x-4'>
+            {/* Card-img  */}
+            <div className='tmb-4 h-24 w-24 flex-shrink-0 rounded'>
+              <img src={Img1} alt="" className='h-24 w-24 object-cover'/>
+            </div>
+            {/* Card-Title  */}
+            <div>
+              <h3 className='text-xl tmb-2 font-semibold text-gray-900'>
+                Medicine / General Physician</h3>
+              <p className='text-sm m-0 pr-8 font-normal text-gray-600'>
+                Cold, flu, fever, vomiting, infections, headaches or any other general health issues
+              </p>
+            </div>
+        </div>
+        </div>
+     
+        {/* Card-Body-8  */}
+        <div className='tp-4 rounded-[10px] bg-white shadow ring-4 ring-gray-100 ring-offset-2 hover:bg-gray-50 hover:no-underline'>
+        <div className='tmb-5 flex space-x-4'>
+            {/* Card-img  */}
+            <div className='tmb-4 h-24 w-24 flex-shrink-0 rounded'>
+              <img src={Img1} alt="" className='h-24 w-24 object-cover'/>
+            </div>
+            {/* Card-Title  */}
+            <div>
+              <h3 className='text-xl tmb-2 font-semibold text-gray-900'>
+                Medicine / General Physician</h3>
+              <p className='text-sm m-0 pr-8 font-normal text-gray-600'>
+                Cold, flu, fever, vomiting, infections, headaches or any other general health issues
+              </p>
+            </div>
+        </div>
+        </div>
+     
+        {/* Card-Body-9  */}
+        <div className='tp-4 rounded-[10px] bg-white shadow ring-4 ring-gray-100 ring-offset-2 hover:bg-gray-50 hover:no-underline'>
+        <div className='tmb-5 flex space-x-4'>
+            {/* Card-img  */}
+            <div className='tmb-4 h-24 w-24 flex-shrink-0 rounded'>
+              <img src={Img1} alt="" className='h-24 w-24 object-cover'/>
+            </div>
+            {/* Card-Title  */}
+            <div>
+              <h3 className='text-xl tmb-2 font-semibold text-gray-900'>
+                Medicine / General Physician</h3>
+              <p className='text-sm m-0 pr-8 font-normal text-gray-600'>
+                Cold, flu, fever, vomiting, infections, headaches or any other general health issues
+              </p>
+            </div>
+        </div>
+        </div>
+     
+        {/* Card-Body-10  */}
+        <div className='tp-4 rounded-[10px] bg-white shadow ring-4 ring-gray-100 ring-offset-2 hover:bg-gray-50 hover:no-underline'>
+        <div className='tmb-5 flex space-x-4'>
+            {/* Card-img  */}
+            <div className='tmb-4 h-24 w-24 flex-shrink-0 rounded'>
+              <img src={Img1} alt="" className='h-24 w-24 object-cover'/>
+            </div>
+            {/* Card-Title  */}
+            <div>
+              <h3 className='text-xl tmb-2 font-semibold text-gray-900'>
+                Medicine / General Physician</h3>
+              <p className='text-sm m-0 pr-8 font-normal text-gray-600'>
+                Cold, flu, fever, vomiting, infections, headaches or any other general health issues
+              </p>
+            </div>
+        </div>
+        </div>
+     
+        {/* Card-Body-11  */}
+        <div className='tp-4 rounded-[10px] bg-white shadow ring-4 ring-gray-100 ring-offset-2 hover:bg-gray-50 hover:no-underline'>
+        <div className='tmb-5 flex space-x-4'>
+            {/* Card-img  */}
+            <div className='tmb-4 h-24 w-24 flex-shrink-0 rounded'>
+              <img src={Img1} alt="" className='h-24 w-24 object-cover'/>
+            </div>
+            {/* Card-Title  */}
+            <div>
+              <h3 className='text-xl tmb-2 font-semibold text-gray-900'>
+                Medicine / General Physician</h3>
+              <p className='text-sm m-0 pr-8 font-normal text-gray-600'>
+                Cold, flu, fever, vomiting, infections, headaches or any other general health issues
+              </p>
+            </div>
+        </div>
+        </div>
+     
+        {/* Card-Body-12  */}
+        <div className='tp-4 rounded-[10px] bg-white shadow ring-4 ring-gray-100 ring-offset-2 hover:bg-gray-50 hover:no-underline'>
+        <div className='tmb-5 flex space-x-4'>
+            {/* Card-img  */}
+            <div className='tmb-4 h-24 w-24 flex-shrink-0 rounded'>
+              <img src={Img1} alt="" className='h-24 w-24 object-cover'/>
+            </div>
+            {/* Card-Title  */}
+            <div>
+              <h3 className='text-xl tmb-2 font-semibold text-gray-900'>
+                Medicine / General Physician</h3>
+              <p className='text-sm m-0 pr-8 font-normal text-gray-600'>
+                Cold, flu, fever, vomiting, infections, headaches or any other general health issues
+              </p>
+            </div>
+        </div>
+        </div>
+     
+        {/* Card-Body-13  */}
+        <div className='tp-4 rounded-[10px] bg-white shadow ring-4 ring-gray-100 ring-offset-2 hover:bg-gray-50 hover:no-underline'>
+        <div className='tmb-5 flex space-x-4'>
+            {/* Card-img  */}
+            <div className='tmb-4 h-24 w-24 flex-shrink-0 rounded'>
+              <img src={Img1} alt="" className='h-24 w-24 object-cover'/>
+            </div>
+            {/* Card-Title  */}
+            <div>
+              <h3 className='text-xl tmb-2 font-semibold text-gray-900'>
+                Medicine / General Physician</h3>
+              <p className='text-sm m-0 pr-8 font-normal text-gray-600'>
+                Cold, flu, fever, vomiting, infections, headaches or any other general health issues
+              </p>
+            </div>
+        </div>
+        </div>
+     
+        {/* Card-Body-14  */}
+        <div className='tp-4 rounded-[10px] bg-white shadow ring-4 ring-gray-100 ring-offset-2 hover:bg-gray-50 hover:no-underline'>
+        <div className='tmb-5 flex space-x-4'>
+            {/* Card-img  */}
+            <div className='tmb-4 h-24 w-24 flex-shrink-0 rounded'>
+              <img src={Img1} alt="" className='h-24 w-24 object-cover'/>
+            </div>
+            {/* Card-Title  */}
+            <div>
+              <h3 className='text-xl tmb-2 font-semibold text-gray-900'>
+                Medicine / General Physician</h3>
+              <p className='text-sm m-0 pr-8 font-normal text-gray-600'>
+                Cold, flu, fever, vomiting, infections, headaches or any other general health issues
+              </p>
+            </div>
+        </div>
+        </div>
+     
+        {/* Card-Body-15  */}
+        <div className='tp-4 rounded-[10px] bg-white shadow ring-4 ring-gray-100 ring-offset-2 hover:bg-gray-50 hover:no-underline'>
+        <div className='tmb-5 flex space-x-4'>
+            {/* Card-img  */}
+            <div className='tmb-4 h-24 w-24 flex-shrink-0 rounded'>
+              <img src={Img1} alt="" className='h-24 w-24 object-cover'/>
+            </div>
+            {/* Card-Title  */}
+            <div>
+              <h3 className='text-xl tmb-2 font-semibold text-gray-900'>
+                Medicine / General Physician</h3>
+              <p className='text-sm m-0 pr-8 font-normal text-gray-600'>
+                Cold, flu, fever, vomiting, infections, headaches or any other general health issues
+              </p>
+            </div>
+        </div>
+        </div>
+     
+
       </div>
     </div>
   );
