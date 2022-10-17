d0c59485623a46fcb604c10ce91574014dcbcbb2
ShamimHosen75 <developer.shamimhosen@gmail.com>
2022-10-17 16:26:57 +0600

    images added

---
+++ src/components/Home components/Specialties/Specialties.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Specialties = () => {
+  return (
+    <div>
+      <div className='text-4xl font-bold pt-9 pb-9'>
+        <h1>Please select a speciality</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Specialties;
