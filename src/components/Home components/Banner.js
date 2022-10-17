6ef8e86fbc0d3e9968b89419ba8ab88445949634
Emranul_farhad <emranulfarhad@gmail.com>
2022-10-17 19:07:36 +0600

    banner part done

--- src/components/Home components/Banner.js
+++ src/components/Home components/Banner.js
@@ -49,6 +49,36 @@ const Banner = () => {
           </div>
         </SwiperSlide>
 
+        
+        {/* our second banner */}
+        <SwiperSlide className="bg-[#E5F1FB]">
+          <div className="flex justify-between pt-3">
+            <div className="scbgdraw" >  </div>
+            <div className="scbgdraw2 mt-10 " > </div>
+            <div className="scbgdraw3" > </div>
+            </div>
+          <div className="flex flex-col md:flex-row items-center ">
+            <div className="w-1/2">
+              <img className="" src={scbannerimage} alt="" />
+            </div>
+            <div className="flex w-1/2 justify-end mt-5 md:mt-0" >
+              <div> 
+                <p className="text-4xl uppercase font-bold text-left">meet  Elizabeth Blackwell 
+                <br /> Our american  <span className="text-[#7FB7ED]">gynaecologist</span> specialist </p>
+                <div className="flex mt-5 md:mt-0">
+                <button className="btn btn-outline btn-primary mt-3 text-left align-left ">
+                    visit Doctors{" "}
+                  </button> 
+                </div>
+              </div>
+             
+            </div>
+          </div>
+        </SwiperSlide>
+
+
+
+
       </Swiper>
     </div>
   );
