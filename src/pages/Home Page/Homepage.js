6f7a3a974b50857eed1ca227eb26ca5d1f5fb35b
Emranul_farhad <emranulfarhad@gmail.com>
2022-10-17 21:22:48 +0600

    what are you looking part done

--- src/pages/Home Page/Homepage.js
+++ src/pages/Home Page/Homepage.js
@@ -1,22 +1,24 @@
 // here we will add all of our home page components file if you cannot understand please read our componennts text.txt file
 
-
-import React from 'react'
-import AvailableFeatures from '../../components/Home components/AvailableFeatures/AvailableFeatures'
-import Banner from '../../components/Home components/Banner'
-import { Use } from '../../components/Home components/use/Use'
+import React from "react";
+import AvailableFeatures from "../../components/Home components/AvailableFeatures/AvailableFeatures";
+import Banner from "../../components/Home components/Banner";
+import { Use } from "../../components/Home components/use/Use";
+import Services from "../../components/Home components/What are u looking fr/Services";
 
 const Homepage = () => {
   return (
     <div>
-        {/* banner  */}
-        <Banner></Banner>
-        {/* what are you looking */}
-
-        
+      {/* banner  */}
+      <Banner></Banner>
+      {/* what are you looking */}
+      <div className="mt-[100px]">
+        <Services></Services>
+      </div>
+    {/* dffjjfj */}
+    
     </div>
+  );
+};
 
-  )
-}
-
-export default Homepage
+export default Homepage;
