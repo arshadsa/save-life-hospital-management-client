15af1e1f59a2206dc619f30701cf86e4acbb6dd7
Arshad <arshad.syed.anwar@gmail.com>
2022-10-18 23:51:10 +0600

    enabling routes in the code

--- src/App.js
+++ src/App.js
@@ -1,37 +1,19 @@
-import './App.css';
-import { Footer } from './shared/Footer/Footer';
-import AvailableFeatures from './components/Home components/AvailableFeatures/AvailableFeatures';
-import Doctor from './components/Home components/Book Our Doctors/Doctor';
-import Specialties from './components/Home components/Specialties/Specialties';
-import { Use } from './components/Home components/use/Use';
-import Homepage from './pages/Home Page/Homepage';
-import Chat from './components/Msngrchat/Chat';
-import { NewsLetter } from './components/Home components/newsletter/NewsLetter';
-
-
-
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Footer } from "./shared/Footer/Footer";
+import Homepage from "./pages/Home Page/Homepage";
+import Chat from "./components/Msngrchat/Chat";
 
 function App() {
-
   return (
     <div className="App">
-      <Homepage></Homepage>
-      <AvailableFeatures></AvailableFeatures>
-      <Specialties></Specialties>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <Use></Use>
-      <Doctor></Doctor>
-      <div className='mb-[150px]'>
+      <Routes>
+        <Route path="/" element={<Homepage></Homepage>}></Route>
+      </Routes>
+
+      <div className="mb-[150px]">
         <Chat></Chat>
       </div>
-      <NewsLetter></NewsLetter>
       <Footer></Footer>
     </div>
   );
