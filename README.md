e0d1b43a59b6ae9f28b88ffa00aaee63b6010481
Arshad <arshad.syed.anwar@gmail.com>
2022-10-19 20:26:22 +0600

    reverse

--- README.md
+++
@@ -1,1 +0,0 @@
-<h1>Savelife.com</h1>
--- src/App.js
+++ src/App.js
@@ -1,51 +1,20 @@
-import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Footer } from "./shared/Footer/Footer";
 import Homepage from "./pages/Home Page/Homepage";
 import Chat from "./components/Msngrchat/Chat";
-import AddDoctors from "./components/adddoctors/AddDoctors";
-
 
 function App() {
-  const router = createBrowserRouter([
-    { path: "/", element: (<Homepage></Homepage>),},
-    { path: "/adddoctors", element: ( <AddDoctors></AddDoctors>),},
-    {
-      path: "/doctor",
-      element: (
-        <Speciality></Speciality>
-      ),
-    },
-    {
-      path: "/doctor/add",
-      element: (
-       <AddDoctors></AddDoctors>
-      ),
-    },
-    {
-      path: "/doctor/all",
-      element: (
-        <AllDoctors></AllDoctors>
-      ),
-    },
-    {
-      path: "/doctor/:id",
-      element: (
-        <AllDoctors></AllDoctors>
-      ),
-    },
-   
-    
-  ]);
   return (
     <div className="App">
-      
-      <RouterProvider router={router} />
-      <Footer></Footer>
-      {/* <div className="mb-[150px]">
+      <Routes>
+        <Route path="/" element={<Homepage></Homepage>}></Route>
+      </Routes>
+
+      <div className="mb-[150px]">
         <Chat></Chat>
       </div>
-      <Footer></Footer> */}
+      <Footer></Footer>
     </div>
   );
 }
