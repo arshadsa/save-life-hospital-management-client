a11d4bead95d89202ee3f585378649bbbf6d4345
AnikHaque <ekramul15-811@diu.edu.bd>
2022-10-19 12:05:14 +0600

    added router 6.4.2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import { Route, Routes } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
 import "./App.css";
 import { Footer } from "./shared/Footer/Footer";
 import Homepage from "./pages/Home Page/Homepage";
 import Chat from "./components/Msngrchat/Chat";
 
+
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+       <Homepage></Homepage>
+      ),
+    },
+    
+    
+  ]);
   return (
     <div className="App">
       
-
+      <RouterProvider router={router} />
+      <Footer></Footer>
       {/* <div className="mb-[150px]">
         <Chat></Chat>
       </div>
