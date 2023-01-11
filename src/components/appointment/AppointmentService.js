ff0cea2ecf0c4220bf0e2624e1abcf3f7025db27
AnikHaque <ekramul15-811@diu.edu.bd>
2023-01-12 01:01:37 +0600

    added

--- src/components/appointment/AppointmentService.js
+++ src/components/appointment/AppointmentService.js
@@ -2,7 +2,10 @@ import moment from 'moment-timezone';
 import React from 'react';
 const AppointmentService = ({ service, setTreatment, date }) => {
   const { name, availableSlots } = service;
-  
+  console.log("selected date", moment(date).tz("Asia/Dhaka").format('L'));
+  console.log("availableSlots", availableSlots);
+  let d = moment(date).tz("Asia/Dhaka").format('L')
+  console.log("availableSlots on selected date", availableSlots[d]);
   // console.log("formating date with moment js", moment(availableSlots[date]).format('l'));
   return (
     <>
@@ -11,17 +14,17 @@ const AppointmentService = ({ service, setTreatment, date }) => {
           <div className="card-body mx-auto">
             <h2 className="card-title text-primary">{name}</h2>
             <p>
-              {availableSlots?.length
+              {availableSlots[d]?.length
                 ? null
                 :
                 <span className='text-red-500'>Try another day !!</span>
 
               }
             </p>
-            <p>{availableSlots?.length} {availableSlots?.length > 1 ? 'spaces' : 'space'} available</p>
+            <p>{availableSlots[d]?.length} {availableSlots[d]?.length > 1 ? 'spaces' : 'space'} available</p>
             <div className="card-actions justify-center">
               <label
-                disabled={availableSlots?.length === 0}
+                disabled={availableSlots[d]?.length === 0}
                 htmlFor="booking-modal"
                 onClick={() => setTreatment(service)}
                 className="btn btn-primary">
